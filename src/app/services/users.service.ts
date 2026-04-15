2f88437b15906b93062f4dc9bf288cbdb2d866ad
mohammedAdelAv <medoadel029130@gmail.com>
2026-04-15 19:40:40 +0200

    update users serv

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -10,43 +10,64 @@ import { Users } from '../core/models/users';
 
 export class UsersService {
 
-  private usersUrl = 'http://localhost:3001/users';
+  // private usersUrl = 'http://localhost:3001/users';
+  private mockApiUrl = 'https://69dfb1f7d6de26e11929bcaa.mockapi.io/users';
 
   constructor(private http: HttpClient) { }
 
   //----------------------------------------------------------------------------------
 
   // get data
-  get(): Observable<Users> {
-    return this.http.get<Users>(this.usersUrl);
+  // get(): Observable<Users> {
+  //   return this.http.get<Users>(this.usersUrl);
+  // }
+
+  get(): Observable<Users[]> {
+    return this.http.get<Users[]>(this.mockApiUrl);
   }
+
 
   //----------------------------------------------------------------------------------
 
   // add data
-  post(objForm: Users): Observable<Users> {
-    return this.http.post<Users>(this.usersUrl, objForm);
+  // post(objForm: Users): Observable<Users> {
+  //   return this.http.post<Users>(this.usersUrl, objForm);
+  // }
+
+  post(user: Users): Observable<Users> {
+    return this.http.post<Users>(this.mockApiUrl, user);
   }
 
   //----------------------------------------------------------------------------------
 
   // get data by id
+  // getById(id: any): Observable<Users> {
+  //   return this.http.get<Users>(this.usersUrl + `/${id}`);
+  // }
+
   getById(id: any): Observable<Users> {
-    return this.http.get<Users>(this.usersUrl + `/${id}`);
+    return this.http.get<Users>(`${this.mockApiUrl}/${id}`);
   }
 
   //----------------------------------------------------------------------------------
 
   // delete data by id
-  delete(id: any): Observable<Users> {
-    return this.http.delete<Users>(this.usersUrl + `/${id}`);
-  }
+  // delete(id: any): Observable<Users> {
+  //   return this.http.delete<Users>(this.usersUrl + `/${id}`);
+  // }
 
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${this.mockApiUrl}/${id}`);
+  }
   //----------------------------------------------------------------------------------
 
   // update data by id
-  put(objForm: Users, id: any): Observable<Users> {
-    return this.http.put<Users>(this.usersUrl + `/${id}`, objForm);
+  // put(objForm: Users, id: any): Observable<Users> {
+  //   return this.http.put<Users>(this.usersUrl + `/${id}`, objForm);
+  // }
+
+  put(user: Users, id: any): Observable<Users> {
+    return this.http.put<Users>(`${this.mockApiUrl}/${id}`, user);
   }
 
   //----------------------------------------------------------------------------------
